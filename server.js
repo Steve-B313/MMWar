f62caa2731803e3455622f4c5bb9b529dd1a4be5
Mustafa Al-Baghdadi <Mustafa Al-Baghdadi>
2017-07-18 12:34:50 +0300

    rollback

--- server.js
+++ server.js
@@ -31,15 +31,23 @@ var connectionString = "postgres://srgvuycakcyozi:6f209485fdb080e2ed9f8fda6226b3
 
 //Callback for the 1st connection
 io.on('connection', function (socket) {
-    
-    console.log('connected to server');
 
     //Player id
     var thisPlayerId;
     
-    //The client pressed MultiPlayer button
-	socket.on('register', function(playerData) {
-        console.log('getting id');
+    //You are the first player, create the match
+    if (counter == 0) {
+        thisMatchId = shortid.generate();
+        match = thisMatchId.toString();
+        matches[thisMatchId] = match;
+    } 
+    socket.room = match;
+    socket.join(match);
+    
+    //Tell the player that he connected and ask him for his Id
+    socket.emit('register', { match: thisMatchId, camp: counter });
+    //The client recieve the register callback and sends his id
+	socket.on('playerId', function(playerData) {
         if (playerData.id) {//There is a previous id
             thisPlayerId = playerData.id;
             console.log('there is a previously existing id: ', playerData.id);
@@ -56,7 +64,6 @@ io.on('connection', function (socket) {
                     .query(select)
                     .on('row', function (row) {
                         console.log(JSON.stringify(row));
-                        socket.emit('playerInfo', { id: row.id, name: row.name, played: row.played, won: row.won });
                     });
             });
         } else {
@@ -69,7 +76,7 @@ io.on('connection', function (socket) {
                 console.log('connected to postgres for inserting');
                 const insert = {
                     text: 'INSERT INTO user_db(id, name, played, won) VALUES($1, $2, $3, $4);',
-                    values: [thisPlayerId, playerData.name, playerData.played, playerData.won],
+                    values: [thisPlayerId, playerData.name, 0, 0],
                 };
 				client
                     .query(insert)
@@ -82,29 +89,13 @@ io.on('connection', function (socket) {
         pg.end();
     });
     
-    //The client pressed FindMatch button
-    socket.on('findMatch', function (data) {
-        console.log('finding match');
-        
-        //You are the first player, create the match
-        if (counter == 0) {
-            thisMatchId = shortid.generate();
-            match = thisMatchId.toString();
-            matches[thisMatchId] = match;
-        } 
-        socket.room = match;
-        socket.join(match);
+    //You are the second player, echo the start command
+    if (counter != 0) {
+        io.in(socket.room).emit('ready', { match: thisMatchId });
+    }
+    counter++;
+    counter = counter % 2;
 
-        //You are the second player, echo the start command
-        if (counter != 0) {
-            io.in(socket.room).emit('ready', { match: thisMatchId });
-        }
-        counter++;
-        counter = counter % 2;
-
-        socket.emit('findMatch', { match: thisMatchId, camp: counter });
-    });
-    
     //Broadcast to both players the attack
     socket.on('attack', function (data) {
         console.log('attack ', JSON.stringify(data));
